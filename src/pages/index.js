223a50bbb3a0c8105beff0320fbdc482ce798448
Anni Kynsilehto <anni.kynsilehto@gmail.com>
2020-01-02 21:34:24 +0200

    Gatsby tutorials 0-3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Layout from "../components/layout"
+export default () => (
+	<Layout>
+		<div style={{ color: "purple", fontFamily: "Helvetica" }}>
+			<Header headerText="Hello Gatsby!" />
+			<p>What a world.</p>
+			<img src="https://source.unsplash.com/random/400x200" alt="" />
+		</div>
+	</Layout>
+)
